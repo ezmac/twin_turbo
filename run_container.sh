ae35f65c6b09f332104a59b099901894619cbfa1
Tad Merchant <system.root@gmail.com>
2015-05-23 15:06:57 -0400

    removed socket ambassador dependency

--- run_container.sh
+++ run_container.sh
@@ -5,10 +5,10 @@ pwd=$(printf '%q\n' "${PWD##*/}")
 if [ -z $env ]; then
   echo "PROD"
   #prod
-  docker run --name nginx_front -i -t -p 80:80 -p 443:443 -v /data/logs:/data/logs:rw -v /data/containers/nginx_front/config/:/data/config/:rw --volumes-from socket_ambassador $pwd
+  docker run --name nginx_front -i -t -p 80:80 -p 443:443 -v /data/logs:/data/logs:rw -v /data/containers/nginx_front/config/:/data/config/:rw $pwd
 elif [[ $env="dev" ]]; then
   #dev
   echo $env
-  docker run --name nginx_front -i -t -p 8088:80  -v `pwd`/logs/:/data/logs/:rw --volumes-from socket_ambassador -v `pwd`/config/:/data/config/:rw $pwd
+  docker run --name nginx_front -i -t -p 8088:80  -v `pwd`/logs/:/data/logs/:rw -v `pwd`/config/:/data/config/:rw $pwd
 fi
 
